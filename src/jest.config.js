f59097d895d63def0dc3d27b4b16cc50af954ba2
Daan Thelen <daan.thelen0@gmail.com>
2024-04-03 16:23:52 +0200

    Added circleci config.yml file.

--- src/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @returns {Promise<import('jest').Config>} */
-module.exports = async () => {
-    return {
-      verbose: true,
-      transformIgnorePatterns: [
-        '/node_modules/(?!axios/)'
-        ],
-    };
-  };
