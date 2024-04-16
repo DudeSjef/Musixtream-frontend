b7b0c3f87432f7e7a16d80cdcf32085515aff2ed
Daan Thelen <daan.thelen0@gmail.com>
2024-04-16 16:16:08 +0200

    Changed ImagePullPolicy to Always

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   const [testData, setTestData] = useState("");
 
   useEffect(() => {
+    TestAPI.log();
     TestAPI.test().then(data => setTestData(data));
   }, []);
 
--- src/apis/TestAPI.js
+++ src/apis/TestAPI.js
@@ -4,7 +4,8 @@ const BASE_URL = `http://${process.env.REACT_APP_MUSIXTREAM_BACKEND_SERVICE}/`;
 
 const TestAPI = {
     test: () => axios.get(BASE_URL)
-        .then(response => response.data)
+        .then(response => response.data),
+    log: () => console.log(`Base URL is ${BASE_URL}`)
 };
 
 export default TestAPI;
